d6f3af3347aaa5219f84d7c23a0c088c6f8810dd
Tuyen <ngotrungtuyen080703@gmail.com>
2025-03-19 23:32:16 +0700

    update home

---
+++ app/src/main/java/com/example/audiobook/activities/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.audiobook.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.audiobook.R;
+
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
