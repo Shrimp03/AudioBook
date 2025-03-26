85a176f50a69e58e57d3b5de63e196316b101843
Tuyen <ngotrungtuyen080703@gmail.com>
2025-03-26 23:30:12 +0700

    update config fragment for home page

---
+++ app/src/main/java/com/example/audiobook/fragments/HomeFragment.java
@@ -0,0 +1,7 @@
+package com.example.audiobook.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+}
---
+++ app/src/main/java/com/example/audiobook/fragments/LibraryFragment.java
@@ -0,0 +1,4 @@
+package com.example.audiobook.fragments;
+
+public class LibraryFragment {
+}
