8d53b64c93207ae980144800c6f84d799d5494c4
anhnq125 <anhnq125@fpt.com>
2025-04-09 14:10:55 +0700

    first_personalize

---
+++ app/src/main/java/com/example/audiobook/adapters/PersonalizeAdapter.java
@@ -0,0 +1,54 @@
+package com.example.audiobook.adapters;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import com.example.audiobook.fragments.PersonalizeItemFragment;
+import com.example.audiobook.models.Category;
+import java.util.List;
+
+public class PersonalizeAdapter extends FragmentStateAdapter {
+    private List<Category> categories;
+
+    public PersonalizeAdapter(FragmentActivity fragmentActivity, List<Category> categories) {
+        super(fragmentActivity);
+        this.categories = categories;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3; // 3 trang
+    }
+
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return PersonalizeItemFragment.newInstance(
+                        PersonalizeItemFragment.TYPE_INTRO,
+                        "Welcome !",
+                        "Find what you are looking for",
+                        "By personalize your account, we can help you to find what you like.",
+                        categories
+                );
+            case 1:
+                return PersonalizeItemFragment.newInstance(
+                        PersonalizeItemFragment.TYPE_CATEGORIES,
+                        "",  // Không cần subtitle cho trang Categories
+                        "Chọn danh mục yêu thích",
+                        "",
+                        categories
+                );
+            case 2:
+                return PersonalizeItemFragment.newInstance(
+                        PersonalizeItemFragment.TYPE_FINISH,
+                        "Congratulations !",
+                        "Hoàn tất!",
+                        "Bạn đã chọn xong các danh mục yêu thích. Bắt đầu trải nghiệm ngay nào!",
+                        categories
+                );
+            default:
+                throw new IllegalStateException("Invalid position");
+        }
+    }
+}
--- app/src/main/java/com/example/audiobook/models/Category.java
+++ app/src/main/java/com/example/audiobook/models/Category.java
@@ -3,7 +3,10 @@ package com.example.audiobook.models;
 public class Category {
     private int id;
     private String name;
-
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
     public int getId() {
         return id;
     }
