2ab773c13e3b93419becfa75f6dbdf1109232337
quyetbh-Its <quyetbh@iceteasoftware.com>
2025-04-10 17:00:17 +0700

    login and register

--- app/src/main/java/com/example/audiobook/api/CoreAppInterface.java
+++ app/src/main/java/com/example/audiobook/api/CoreAppInterface.java
@@ -1,12 +1,22 @@
 package com.example.audiobook.api;
 
+import com.example.audiobook.dto.LoginDTO;
+import com.example.audiobook.dto.RegisterDTO;
 import com.example.audiobook.models.Audiobook;
 import com.example.audiobook.models.Category;
+import com.example.audiobook.response.RegisterResponse;
+import com.example.audiobook.response.UserLoginResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -14,6 +24,12 @@ public interface CoreAppInterface {
 
     @GET(APIconst.GET_ALL_CATE)
     Call<List<Category>> getAllCategory();
+
+    @POST(APIconst.REGISTER)
+    Call<RegisterResponse> registerAccount(@Body RegisterDTO registerDTO);
+
+    @POST(APIconst.LOGIN)
+    Call<UserLoginResponse> loginAccount(@Body LoginDTO loginDTO);
 
     @GET(APIconst.GET_ALL_AUDIO_BOOKS)
     Call<List<Audiobook>> getAllAudioBooks();
@@ -26,5 +42,15 @@ public interface CoreAppInterface {
 
     @GET(APIconst.GET_AUDIO_BOOKS_BY_USER)
     Call<List<Audiobook>> getAudioBooksByUserId(@Path("userId") String userId);
+
+    Gson gson = new GsonBuilder()
+            .setDateFormat("yyyy-MM-dd HH:mm:ss")
+            .create();
+
+    CoreAppInterface coreAppInterface = new Retrofit.Builder()
+            .baseUrl(APIconst.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(CoreAppInterface.class);
 
 }
---
+++ app/src/main/java/com/example/audiobook/viewmodel/RegisterViewModel.java
@@ -0,0 +1,55 @@
+package com.example.audiobook.viewmodel;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.audiobook.api.CoreAppInterface;
+import com.example.audiobook.dto.RegisterDTO;
+import com.example.audiobook.response.RegisterResponse;
+import java.util.Objects;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RegisterViewModel extends ViewModel {
+    public MutableLiveData<String> toast = new MutableLiveData<>();
+    private static final String TAG = "RegisterViewModel";
+    public MutableLiveData<Boolean> registrationSuccess = new MutableLiveData<>();
+
+    public void registerUser(String email, String password, String dateOfBirth){
+        RegisterDTO registerDTO = new RegisterDTO(email,password,dateOfBirth);
+
+        CoreAppInterface.coreAppInterface.registerAccount(registerDTO).enqueue(new Callback<RegisterResponse>() {
+            @Override
+            public void onResponse(Call<RegisterResponse> call, Response<RegisterResponse> response) {
+                System.out.println(response);
+                Log.d(TAG, "onResponse called."+ response);
+                Log.d(TAG, "Response raw: " + response.toString());
+                Log.d(TAG, "Response code: " + response.code());
+                Log.d(TAG, "Response successful: " + response.isSuccessful());
+                try {
+                    if (response.isSuccessful()) {
+                        RegisterResponse responseData = response.body();
+                        Log.d(TAG, "Response body: " + responseData.toString());
+                        if (Objects.equals(responseData.getStatus(), "ok")) {
+                            toast.setValue(responseData.getMessage());
+                            registrationSuccess.setValue(true);
+                        } else if (response.body() != null && response.code() == 226) {
+                        }
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+            @Override
+            public void onFailure(@NonNull Call<RegisterResponse> call, @NonNull Throwable t) {
+                toast.setValue("false");
+                registrationSuccess.setValue(false);
+
+            }
+        });
+    }
+}
