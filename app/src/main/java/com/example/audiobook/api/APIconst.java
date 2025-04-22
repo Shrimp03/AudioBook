e5d235ed4a19e40623a4127fcde4d7eeb8b44e3a
quyetbh-Its <quyetbh@iceteasoftware.com>
2025-04-22 16:22:32 +0700

    fix ui login and register

--- app/src/main/java/com/example/audiobook/api/APIconst.java
+++ app/src/main/java/com/example/audiobook/api/APIconst.java
@@ -1,13 +1,13 @@
 package com.example.audiobook.api;
 
 public class APIconst {
-    public static final String BASE_URL = "https://3a85-14-0-22-145.ngrok-free.app";
+    public static final String BASE_URL = " https://d3ef-14-0-22-145.ngrok-free.app";
 
     /// uri
     public static final String GET_ALL_CATE = BASE_URL + "/api/category/getAll";
     public static final String GET_ALL_AUDIO_BOOKS = BASE_URL + "/api/audiobooks/getAll";
 
-    public static final String REGISTER = BASE_URL + "/user/register";
+    public static final String REGISTER = BASE_URL + "api/user/register";
 
     public static final String LOGIN = BASE_URL + "/auth/token/get";
 
